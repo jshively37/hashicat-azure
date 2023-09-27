871d93b036b67d269881a935e6b441a891cd4c29
Jeff Shively <jeffrey.shively@hashicorp.com>
2023-09-27 18:11:46 +0000

    Adding first tag

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "jeff-arrow-org"
     workspaces {
       name = "hashicat-azure"
